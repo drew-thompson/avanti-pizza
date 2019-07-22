1dda1250cf9b366db168d8fe14860fcd854f1726
Drew Thompson <drew@commongenius.com>
2019-07-22 13:01:53 -0700

    refactor(*): update tests

--- libs/shared/ui/src/lib/pizza/pizza.component.spec.ts
+++ libs/shared/ui/src/lib/pizza/pizza.component.spec.ts
@@ -1,21 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Pizza } from '@avanti-pizza/api-interface';
 import { PizzaComponent } from './pizza.component';
 
 describe('PizzaComponent', () => {
   let component: PizzaComponent;
+  const completePizza: Pizza = {
+    name: 'Garden Veggie',
+    description: 'The veggiest pizza!',
+    pricing: {
+      slice: 1,
+      '12': 2,
+      '14': 3.5,
+      '16': 5,
+      '18': 7.5
+    },
+    toppings: [{ name: 'Bell Peppers', premium: false }],
+    thin: false
+  };
   let fixture: ComponentFixture<PizzaComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PizzaComponent ]
-    })
-    .compileComponents();
+      declarations: [PizzaComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PizzaComponent);
     component = fixture.componentInstance;
+    component.pizza = completePizza;
     fixture.detectChanges();
   });
 
